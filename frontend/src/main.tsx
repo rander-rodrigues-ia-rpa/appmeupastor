8ba0824546415a649cfef2276aaa8c28aaa8f108
Rander Rodrigues <c2620@sebraemg.com.br>
2025-11-26 16:59:29 -0300

    Ajustes css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import './index.css'
-import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
